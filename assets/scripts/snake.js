8c0ef7ee4fe1e2888d551273dce0a339b3245d64
Pwl Webdev <pwl.webdev@gmail.com>
2016-09-27 23:20:42 +0200

    Food generation, snake growth and stop conditons completed

--- assets/scripts/snake.js
+++ assets/scripts/snake.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	var board = initBoard();
 	//console.log(board);
 	render(board);
+	createFood(board);
 
 	$(window).keypress(function(event) {
   		//console.log( "Handler for .keypress() called."+event.key);
@@ -14,29 +15,47 @@ $(document).ready(function(){
 
 var size = 40;
 var middle = 19;
-var timeStep = 500;
+var timeStep = 10;
 var animation;
 var snake = {
 	position: [middle,middle],
 	direction: "r",
-	tail: [[middle, middle]]
+	tail: []
 };
+var food = {
+	position: [0,0]
+}
 
 function gameLoop(board){
 	var end = false;
-	//while(!end){
 		animation = window.setInterval(function(){
 			if(move(board)){
 				render(board);
 			}
 		}, timeStep);
-	//}
 }
 
 function stopGame(){
 	window.clearInterval(animation);
 }
 
+function createFood(board){
+	do{
+		food.position[0] = Math.floor(Math.random()*(size-1));
+		food.position[1] = Math.floor(Math.random()*(size-1));
+	} while(board[food.position[1]][food.position[0]] != " ");
+	console.log("food "+food.position[0]+" "+food.position[1]);
+	board[food.position[1]][food.position[0]] = "x";
+}
+
+function eatFood(board){
+	if(snake.tail.length > 0){
+		snake.tail.push([snake.tail[snake.tail.length-1][0],snake.tail[snake.tail.length-1][1]]);
+	} else{
+		snake.tail.push([snake.position[0],snake.position[1]]);
+	}
+	createFood(board);
+}
 function move(board){
 	var x = snake.position[0];
 	var y = snake.position[1];
@@ -71,11 +90,32 @@ function move(board){
 			break;
 	}
 	console.log(" x "+snake.position[0]+" y "+snake.position[1]);
-	board[y][x] = " ";
+	if(board[snake.position[1]][snake.position[0]] == "x"){
+		eatFood(board);
+	} else if(board[snake.position[1]][snake.position[0]] == "o"){
+		stopGame();
+		return false;
+	}
+
 	board[snake.position[1]][snake.position[0]] = "O";
+	
+	if(snake.tail.length > 0){
+		var temp = snake.tail.pop();
+		board[temp[1]][temp[0]] = " ";
+		temp = [x,y];
+		board[temp[1]][temp[0]] = "o";
+		snake.tail.unshift(temp);
+	} else{
+		board[y][x] = " ";
+	}
 	return true;
 }
-
+/*function drawElements(board){
+	board[snake.position[1]][snake.position[0]] = "O";
+	for(var i = 0; i < snake.tail.length; i++){
+		board[]
+	}
+}*/
 function changeDirection(k){
 	switch(k){
 		case "ArrowUp":
